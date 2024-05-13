8eccc27b77e05f9434bbc255e42a140ff9595a11
Yuri Ageev <ageev@Yuris-MacBook-Air.local>
2024-05-13 15:21:06 +0300

    l29

--- sick-fits/frontend/lib/useForm.js
+++ sick-fits/frontend/lib/useForm.js
@@ -1,8 +1,14 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function useForm(initial = {}) {
   // create a state for our inputs
   const [inputs, setInputs] = useState(initial);
+
+  const intialValues = Object.values(initial).join('');
+
+  useEffect(() => {
+    setInputs(initial);
+  }, [intialValues]);
 
   // {
   //     name: 'wes',
