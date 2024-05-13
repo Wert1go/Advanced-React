70e1612c3fc0ca8bdea368130cc458965d6107a4
Yuri Ageev <ageev@Yuris-MacBook-Air.local>
2024-05-13 19:44:22 +0300

    l34

--- sick-fits/frontend/pages/products/index.js
+++ sick-fits/frontend/pages/products/index.js
@@ -2,12 +2,12 @@ import Pagination from '../../components/Pagination';
 import Products from '../../components/Products';
 
 export default function ProductsPage({ query }) {
-  const page = query.page === 0 ? 1 : query.page;
+  const page = query.page === 0 ? 1 : parseInt(query.page);
 
   return (
     <div>
       <Pagination page={page} />
-      <Products />
+      <Products page={page} />
       <Pagination page={page} />
     </div>
   );
